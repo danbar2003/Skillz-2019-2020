800983d6f7178befa7967a9a5e1f44a43ced41d3
danbar2003 <daniel.bartov@gmail.com>
2019-12-19 01:18:38 +0200

    sad

---
+++ bots/MyBot.java
@@ -0,0 +1,44 @@
+package bots;
+import penguin_game.*;
+
+
+/**
+ * This is an example for a bot.
+ */
+public class MyBot implements SkillzBot {
+    /**
+     * Makes the bot run a single turn.
+     *
+     * @param game - the current game state.
+     */
+    @Override
+    public void doTurn(Game game) {
+        // Go over all of my icebergs.
+        for (Iceberg myIceberg : game.getMyIcebergs()) {
+
+            // The amount of penguins in my iceberg.
+            int myPenguinAmount = myIceberg.penguinAmount;
+
+            // Initializing the iceberg we want to send penguins to.
+            Iceberg destination = null;
+
+            // If there are any neutral icebergs.
+            if (game.getNeutralIcebergs().length > 0) {
+                // Target a neutral iceberg.
+                destination = game.getNeutralIcebergs()[0];
+            } else {
+                // Target an enemy iceberg.
+                destination = game.getEnemyIcebergs()[0];
+            }
+
+            // The amount of penguins the target has.
+            int destinationPenguinAmount = destination.penguinAmount;
+            // If my iceberg has more penguins than the target iceberg.
+            if (myPenguinAmount > destinationPenguinAmount) {
+                // Send penguins to the target.
+                System.out.println(myIceberg + " sends "+ (destinationPenguinAmount + 1) + " penguins to "+ destination);
+                myIceberg.sendPenguins(destination, destinationPenguinAmount + 1);
+            }
+        }
+    }
+}
