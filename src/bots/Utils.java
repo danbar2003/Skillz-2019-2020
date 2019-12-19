141ac3abf803edb514c1f0d1750e03693bc77c7f
Y0URM4MA <noamyag2003@gmail.com>
2019-12-19 02:24:39 +0200

    added closestTo in Utils class. EZPZ

---
+++ src/bots/Utils.java
@@ -0,0 +1,23 @@
+package bots;
+
+import penguin_game.*;
+
+public class Utils {
+
+    public static <T extends MapObject> T closestTo(T object, T[] arr) {
+        if (arr.length > 0) {
+            int distance = object.__distance(arr[0]);
+            T closestObj = arr[0];
+            for (T temp : arr) {
+                if (temp.__distance(object) < distance) {
+                    distance = temp.__distance(object);
+                    closestObj = temp;
+                }
+            }
+            return closestObj;
+        }
+        return null;
+    }
+
+
+}
