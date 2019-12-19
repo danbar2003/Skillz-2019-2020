bbfa4ed4a8be55dbd9d52533a274be8c36d75975
YuvalLevy1 <yuval124levy1@gmail.com>
2019-12-19 09:52:09 +0200

    added getIcebergsUnderAttack

--- src/bots/Utils.java
+++ src/bots/Utils.java
@@ -4,6 +4,7 @@ import penguin_game.*;
 
 public class Utils {
 
+
     public static <T extends MapObject> T closestTo(MapObject object, T[] arr) {
         if (arr.length > 0) {
             int distance = object.__distance(arr[0]);
@@ -19,12 +20,44 @@ public class Utils {
         return null;
     }
 
-    public static int howManyInRange(MapObject mapObject, MapObject[] objects,int range){
+    public static int howManyInRange(MapObject mapObject, MapObject[] objects, int range) {
         int counter = 0;
-        for (MapObject temp: objects) {
+        for (MapObject temp : objects) {
             if (mapObject.__distance(temp) <= range)
                 counter++;
         }
         return counter;
+    }
+
+    public static <T extends GameObject> boolean isInArray(T object, T[] array) {
+        for (T arrayObject : array) {
+            if (arrayObject == object)
+                return true;
+        }
+        return false;
+    }
+
+    public static Iceberg[] getIcebergsUnderAttack(Game game) {
+        Iceberg[] myIcebergs = game.getMyIcebergs();
+        PenguinGroup[] enemyPenguinGroups = game.getEnemyPenguinGroups();
+        Iceberg[] enemyDestinations = new Iceberg[enemyPenguinGroups.length];
+        int counter = 0;
+        for (PenguinGroup penguinGroup : enemyPenguinGroups) {
+            enemyDestinations[counter] = penguinGroup.destination;
+            counter++;
+        }
+        counter = 0;
+        Iceberg[] icebergsUnderAttack = new Iceberg[enemyDestinations.length];
+        for (Iceberg destination : enemyDestinations) {
+            for (int j = 0; j < myIcebergs.length; j++) {
+                if (destination == myIcebergs[j] && !isInArray(myIcebergs[j], icebergsUnderAttack)) {
+                    icebergsUnderAttack[counter] = destination;
+                    System.out.println("help me, turn:" + game.turn + " id:" + destination.id);
+                    counter++;
+                    break;
+                }
+            }
+        }
+        return icebergsUnderAttack;
     }
 }
