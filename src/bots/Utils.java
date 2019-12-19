71c54127f5d6faa0ea0200f392a27f3f2200a0ac
YuvalLevy1 <yuval124levy1@gmail.com>
2019-12-19 09:59:16 +0200

    merged yuval-utils with master

--- src/bots/Utils.java
+++ src/bots/Utils.java
@@ -4,10 +4,9 @@ import penguin_game.*;
 
 public class Utils {
     /**
-     *
      * @param object: Some object in the game.
-     * @param arr: Array of objects (doesn't have to be the same type)
-     * @param <T> :nothing.
+     * @param arr:    Array of objects (doesn't have to be the same type)
+     * @param <T>     :nothing.
      * @return : return the closest object from arr to object parameter, or null if arr length is below 1
      */
     public static <T extends GameObject> T closestTo(GameObject object, T[] arr) {
@@ -25,9 +24,9 @@ public class Utils {
         return null;
     }
 
-    public static int howManyInRange(GameObject GameObject, GameObject[] objects,int range){
+    public static int howManyInRange(GameObject GameObject, GameObject[] objects, int range) {
         int counter = 0;
-        for (GameObject temp: objects) {
+        for (GameObject temp : objects) {
             if (GameObject.__distance(temp) <= range)
                 counter++;
         }
@@ -65,4 +64,6 @@ public class Utils {
         }
         return icebergsUnderAttack;
     }
+
+
 }
