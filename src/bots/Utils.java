28fc743620452b73996190b691e58174125d9ae1
leos <leose@LAPTOP-CA5C77TE>
2019-12-19 10:00:31 +0200

    temp

--- src/bots/Utils.java
+++ src/bots/Utils.java
@@ -3,7 +3,14 @@ package bots;
 import penguin_game.*;
 
 public class Utils {
-
+    public static Game game;
+    /**
+     *
+     * @param object: Some object in the game.
+     * @param arr: Array of objects (doesn't have to be the same type)
+     * @param <T> :nothing.
+     * @return : return the closest object from arr to object parameter, or null if arr length is below 1
+     */
     public static <T extends GameObject> T closestTo(GameObject object, T[] arr) {
         if (arr.length > 0) {
             int distance = object.__distance(arr[0]);
@@ -19,15 +26,18 @@ public class Utils {
         return null;
     }
 
-    public static int howManyInRange(MapObject mapObject, MapObject[] objects,int range){
+    public static int howManyInRange(GameObject GameObject, GameObject[] objects,int range){
         int counter = 0;
-        for (MapObject temp: objects) {
-            if (mapObject.__distance(temp) <= range)
+        for (GameObject temp: objects) {
+            if (GameObject.__distance(temp) <= range)
                 counter++;
         }
         return counter;
     }
 
+    public static void attack(Iceberg iceberg){
+       Iceberg closestBerg = closestTo(iceberg, game.getEnemyIcebergs());
 
-
+        }
+    }
 }
