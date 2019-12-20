fce75428d80395546af85c0ce3ac69cd0f72d523
danbar2003 <daniel.bartov@gmail.com>
2019-12-20 14:25:11 +0200

    Protocol

--- src/bots/missions/AttackWeakest.java
+++ src/bots/missions/AttackWeakest.java
@@ -4,7 +4,7 @@ import bots.Utils;
 import bots.wrapper.MyGame;
 import penguin_game.*;
 
-public class attackWeakest implements Mission<Iceberg> {
+public class AttackWeakest implements Mission<Iceberg> {
 
     @Override
     public State act(Iceberg iceberg){
