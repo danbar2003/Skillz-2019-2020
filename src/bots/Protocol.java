bc5b7dab5db2f861b319444ef8b498cba065ccca
danbar2003 <daniel.bartov@gmail.com>
2019-12-20 12:54:13 +0200

    Protocol

--- src/bots/Protocol.java
+++ src/bots/Protocol.java
@@ -5,7 +5,7 @@ import bots.missions.Mission;
 import java.util.Queue;
 
 public class Protocol<T> implements Mission<T> {
-    public Queue<Mission> protocol;
+    public Queue<Mission<T>> protocol;
 
     public State act(T t){
         return State.FINISHED;
