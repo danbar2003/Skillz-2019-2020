1f422a8b2b6bfa07f2140c871f238a084b51b66e
danbar2003 <daniel.bartov@gmail.com>
2019-12-21 18:32:19 +0200

    cleaned MissionManager

--- src/bots/MissionManager.java
+++ src/bots/MissionManager.java
@@ -7,11 +7,6 @@ public class MissionManager {
 
     public static Protocol<Iceberg> createIcebergMission(Iceberg iceberg){
         Protocol<Iceberg> protocol = new Protocol<>();
-
-        if (Utils.attackingPenguinsComing(iceberg) > Utils.friendlyPenguinsComing(iceberg)) {
-            protocol.add(new SavePenguins());
-            return protocol;
-        }
         return protocol;
     }
 }
