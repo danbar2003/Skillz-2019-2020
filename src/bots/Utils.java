a7faee7e36f8b160008a0c8e0a88a08cc1e2fbd5
YuvalLevy1 <yuval124levy1@gmail.com>
2019-12-22 12:28:56 +0200

    fixed imports

--- src/bots/Utils.java
+++ src/bots/Utils.java
@@ -2,8 +2,11 @@ package bots;
 
 import penguin_game.*;
 
-public class Utils {
+import java.util.LinkedList;
+import java.util.List;
 
+
+public class Utils {
 
     /**
      * @param object is the object you want to find the
@@ -24,4 +27,40 @@ public class Utils {
         }
         return null;
     }
+
+
+    /**
+     * @param iceberg can be an enemy iceberg or a neutral iceberg
+     * @return the enemy's penguinGroups that their destination is "iceberg"
+     */
+    public static List<PenguinGroup> getHelpingPenguinGroupsToIceberg(Game game, Iceberg iceberg) {
+        PenguinGroup[] enemyPenguinGroups = game.getEnemyPenguinGroups();
+        List<PenguinGroup> helpers = new LinkedList<>();
+        for (PenguinGroup enemyPenguinGroup : enemyPenguinGroups) {
+            if (enemyPenguinGroup.destination == iceberg) {
+                helpers.add(enemyPenguinGroup);
+            }
+        }
+        return helpers;
+    }
+
+    /**
+     * @param myIceberg    - the attacker
+     * @param enemyIceberg - the target
+     * @return the amount of penguins the attacker needs to send to the target.
+     */
+    public static int minimumAmountToWin(Game game, Iceberg myIceberg, Iceberg enemyIceberg) {
+        int penguinAmount = enemyIceberg.penguinAmount +
+                enemyIceberg.penguinsPerTurn * myIceberg.getTurnsTillArrival(enemyIceberg);
+        List<PenguinGroup> helpers = getHelpingPenguinGroupsToIceberg(game, enemyIceberg);
+        if (!helpers.isEmpty()) {
+            for (PenguinGroup helper : helpers) {
+                if (helper.turnsTillArrival < myIceberg.getTurnsTillArrival(enemyIceberg)) {
+                    penguinAmount += helper.penguinAmount;
+                }
+            }
+        }
+        return penguinAmount;
+    }
+
 }
