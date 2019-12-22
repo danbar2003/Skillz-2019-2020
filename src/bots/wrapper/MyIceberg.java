d8d2259708483c1b3444a7783c7cbef5217e51bf
danbar2003 <daniel.bartov@gmail.com>
2019-12-23 00:01:38 +0200

    added mission classes

--- src/bots/wrapper/MyIceberg.java
+++ src/bots/wrapper/MyIceberg.java
@@ -1,22 +1,40 @@
 package bots.wrapper;
 
+import java.util.*;
+
+import bots.Utils;
 import penguin_game.*;
 
-public class MyIceberg{
-    public Iceberg iceberg;
-    public int savedPenguins;
 
-    public MyIceberg(Iceberg iceberg){
+public class MyIceberg {
+    public Iceberg iceberg;
+
+    public MyIceberg(Iceberg iceberg) {
         this.iceberg = iceberg;
-        savedPenguins = 0;
     }
 
-    public <T extends GameObject> T closestTo(T[] arr){
+    public boolean canDefendItself(Game game) {
+        List<PenguinGroup> penguinGroups = Arrays.asList(game.getEnemyPenguinGroups());
+        int penguinAmount = iceberg.penguinAmount;
+        PenguinGroup closestEnemyPenguinGroup = Utils.closestTo(iceberg, penguinGroups);
+        for (int i = 0; i < penguinGroups.size(); i++) {
+            if (closestEnemyPenguinGroup.penguinAmount - (
+                    closestEnemyPenguinGroup.turnsTillArrival * iceberg.penguinsPerTurn + penguinAmount) < 0) {
+                return false;
+            }
+            penguinGroups.remove(Utils.closestTo(iceberg, penguinGroups));
+            penguinAmount = penguinAmount + closestEnemyPenguinGroup.turnsTillArrival * iceberg.penguinsPerTurn
+                    - closestEnemyPenguinGroup.penguinAmount;
+        }
+        return true;
+    }
+
+    public <T extends GameObject> T closestTo(T[] arr) {
         if (arr.length > 0) {
             int min = this.iceberg.__distance(arr[0]);
             T obj = arr[0];
             for (T temp : arr) {
-                if (this.iceberg.__distance(temp) < min){
+                if (this.iceberg.__distance(temp) < min) {
                     obj = temp;
                     min = this.iceberg.__distance(temp);
                 }
@@ -24,17 +42,5 @@ public class MyIceberg{
             return obj;
         }
         return null;
-    }
-
-    public void savePenguins(int penguinsAmount){
-        this.savedPenguins = penguinsAmount;
-    }
-
-    public void freePenguins(int penguinsAmount){
-        this.savedPenguins = this.savedPenguins - penguinsAmount;
-    }
-
-    public void freeAllPenguins(){
-        this.savedPenguins = 0;
     }
 }
