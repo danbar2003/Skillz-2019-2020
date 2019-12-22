db91a022277282f73e18e050c6877e286f764236
Daniel <daniel.bartov@gmail.com>
2019-12-22 08:43:14 +0200

    added notMyIcebergs

--- src/bots/wrapper/MyGame.java
+++ src/bots/wrapper/MyGame.java
@@ -1,6 +1,12 @@
 package bots.wrapper;
 
-import penguin_game.*;
+import penguin_game.Game;
+import penguin_game.Iceberg;
+import penguin_game.PenguinGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyGame {
     public static Game game;
@@ -11,24 +17,27 @@ public class MyGame {
     public static Iceberg[] enemyIcebergs;
     public static Iceberg[] allIcebergs;
     public static Iceberg[] neutralIcebergs;
-    //TODO create an notFriendlyIcebergs Iceberg array
+    public static List<Iceberg> notMyIcebergs;
+
 
     //penguin_groups
     public static PenguinGroup[] myPenguinGroups;
     public static PenguinGroup[] enemyPenguinGroups;
     public static PenguinGroup[] allPenguinGroups;
 
-    public static void init(Game game){
+    public static void init(Game game) {
         MyGame.game = game;
     }
 
-    public static void update(Game game){
+    public static void update(Game game) {
         MyGame.turn = game.turn;
 
         MyGame.myIcebergs = game.getMyIcebergs();
         MyGame.enemyIcebergs = game.getEnemyIcebergs();
         MyGame.allIcebergs = game.getAllIcebergs();
         MyGame.neutralIcebergs = game.getNeutralIcebergs();
+        MyGame.notMyIcebergs = Arrays.asList(enemyIcebergs);
+        notMyIcebergs.addAll(Arrays.asList(neutralIcebergs));
 
         MyGame.myPenguinGroups = game.getMyPenguinGroups();
         MyGame.enemyPenguinGroups = game.getEnemyPenguinGroups();
