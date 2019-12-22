4208f79fe414e118d745cdedc321beb01f32ede7
danbar2003 <daniel.bartov@gmail.com>
2019-12-22 19:35:23 +0200

    added mission classes

---
+++ src/bots/missions/AttackWeakestIceberg.java
@@ -0,0 +1,17 @@
+package bots.missions;
+import penguin_game.*;
+
+
+public class AttackWeakestIceberg implements Mission{
+
+    /**
+     * @param game
+     * @param iceberg -
+     * @return returns if the iceberg actually acted
+     */
+    @Override
+    public boolean act(Game game, Iceberg iceberg) {
+        //TODO we need to define what is the weakest Iceberg
+        return true;
+    }
+}
