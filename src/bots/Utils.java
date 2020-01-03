86b8f8016c12aa44f173716b95deaa7c76062211
danbar2003 <daniel.bartov@gmail.com>
2020-01-03 18:45:41 +0200

    added tasks on Utils

--- src/bots/Utils.java
+++ src/bots/Utils.java
@@ -2,11 +2,11 @@ package bots;
 
 import bots.wrapper.MyGame;
 import bots.wrapper.MyIceberg;
+import penguin_game.Game;
 import penguin_game.Iceberg;
 import penguin_game.MapObject;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Utils {
 
@@ -31,5 +31,32 @@ public class Utils {
         for (MyIceberg iceberg : game.getMyIcebergs()){
             iceberg.savePenguins(iceberg.amountToDefend(game));
         }
+    }
+
+    /**
+     * attckers - friendly (ours)
+     * target - enemy iceberg
+     *
+     * @param game - game info
+     * @param attackers - contributing icebergs to attack
+     * @param target - enemy iceberg
+     * @return - map of icebergs who contribute to the attack as keys and
+     * penguin amount that each iceberg is contributing as value
+     */
+    public static Map<MyIceberg, Integer> penguinsFromEachIceberg(MyGame game, List<MyIceberg> attackers, MyIceberg target){
+        //TODO - create this function
+    }
+    /**
+     *
+     * @param game - game info
+     * @return - all opstions to attack each enemy iceberg
+     *           key - target (enemy iceberg)
+     *           value - list of opstions to attack the iceberg
+     *                  value(Map):
+     *                      key - attacking Iceberg
+     *                      value - penguins amount
+     */
+    public static Map<MyIceberg, List<Map<MyIceberg, Integer>>> optionsToAttack(MyGame game){
+        //TODO - create this function after you finished
     }
 }
