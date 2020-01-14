1b2d7f6f1a4d1d92290cfdd4f6030b8b822d8d7a
danbar2003 <daniel.bartov@gmail.com>
2020-01-14 18:04:50 +0200

    .

--- src/bots/MissionManager.java
+++ src/bots/MissionManager.java
@@ -1,13 +1,15 @@
 package bots;
 
 
-import bots.missions.Mission;
-import bots.wrapper.*;
-
 import java.util.*;
 
 
 public class MissionManager {
+    Set<Action> actingActions;
+    public static Set<Action> actionsToMake(){
+
+
+    }
 
 
 }
