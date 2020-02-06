22abe9d1fb0255843121c8b51a0a2ef3a6d6b3b2
leos <leose@LAPTOP-CA5C77TE>
2020-02-06 10:56:02 +0200

    fixed powerSet

--- src/bots/Utils.java
+++ src/bots/Utils.java
@@ -59,26 +59,68 @@ public class Utils {
             mission.calcWaysToExecute();
     }
 
-    public static <T> Set<Set<T>> powerSet(Collection<T> originalSet, int size) {
-        Set<Set<T>> sets = new HashSet<>();
-        if (originalSet.isEmpty()) {
-            sets.add(new HashSet<>());
-            return sets;
-        }
-        List<T> list = new ArrayList<>(originalSet);
-        T head = list.get(0);
-        Set<T> rest = new HashSet<>(list.subList(1, list.size()));
-        for (Set<T> set : powerSet(rest, size)) {
-            if(set.size() <= size-1 ){
-                Set<T> newSet = new HashSet<>();
-                newSet.add(head);
-                newSet.addAll(set);
-                sets.add(newSet);
-                sets.add(set);
+    static int[] nextSet(int[] a, int m) {
+        int n = a.length;
+        int[] currentSet = new int[m];
+        for (int k = 0; k < m; k++)
+            currentSet[k] = a[k];
+        for (int i = m - 1; i >= 0; --i) {
+            if (a[i] < n - m + i ) {
+                a[i]++;
+                for (int j = i + 1; j < m; ++j)
+                    a[j] = a[j - 1] + 1;
+                return currentSet;
+
+
             }
 
         }
+        return currentSet;
 
-        return sets;
     }
+    static int numOfComb(int n, int m) {
+        int k = n - m;
+        if (m > k)
+            m = k;
+        if (m == 0)
+            return 1;
+        int akk1 = k = n + 1 - m, akk2 = 1;
+        k++;
+        for (int i = 2; i <= m; i++, k++) {
+            akk1 *= k;
+            akk2 *= i;
+        }
+        return akk1 / akk2;
+    }
+
+    public static <T> Set<Set<T>> almostPowerSet(List<T> original, int length){
+        int[] a = new int[original.size()];
+        for (int i = 0; i < original.size(); i++)
+            a[i] = i;
+        int combs = numOfComb(original.size(), length);
+        int[][] combinations = new int[combs][length];
+        for (int i = 0; i < combs; i++) {
+            combinations[i] = nextSet(a, length);
+        }
+        Set<Set<T>> allGroups = new HashSet<>();
+        for (int i = 0; i < combs; i++) {
+            Set<T> innerSet = new HashSet<>();
+            for (int j = 0; j < length; j++) {
+                innerSet.add(original.get(combinations[i][j]));
+            }
+            allGroups.add(innerSet);
+        }
+        return allGroups;
+    }
+
+    public static <T> Set<Set<T>> powerSet(Set<T> original, int length){
+        Set<Set<T>> allGroups = new HashSet<>();
+        List<T> originalSet = new LinkedList<>();
+        originalSet.addAll(original);
+        for (int i = 1; i <= length ; i++) {
+            allGroups.addAll(almostPowerSet(originalSet, i));
+        }
+        return allGroups;
+    }
+
 }
